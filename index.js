3803d3a9b45af6891e53b1776fa8e23bfcdbb0c1
Blade Barringer <blade@crookedneighbor.com>
2016-05-21 21:58:25 -0500

    fix: Firefox is unhappy about the shortcuts code when it is minified
    Temporarilly disabling shortcuts and unicode support to deal with it

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var markdownitEmoji = require('markdown-it-emoji');
 
 var parseEmoji = require('./lib/parse-emoji');
 var isBlacklistedEmoji = require('./lib/is-blacklisted-emoji');
-var shortcuts = require('./lib/shortcuts');
+// var shortcuts = require('./lib/shortcuts');
 var customEmojis = require('./lib/custom-emojis');
 
 // https://github.com/markdown-it/markdown-it-emoji/tree/4d5f6af1b6efb0975dae2ac51dbe6252636724aa#change-output
 function emojiPlugin (md) {
   md.use(markdownitEmoji, {
     defs: customEmojis,
-    shortcuts: shortcuts,
+    // shortcuts: shortcuts,
   });
 
   md.renderer.rules.emoji = function markdownEmojiRules (token, idx) {
