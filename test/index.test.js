6b6f633a75c92650631b3190a3d5290aa67278b2
Blade Barringer <blade@crookedneighbor.com>
2016-05-29 09:09:55 -0500

    fix: Correct issue with Firefox incompatibility
    Closes #1

--- test/index.test.js
+++ test/index.test.js
@@ -13,13 +13,13 @@ describe('habitica-markdown-emoji', () => {
     expect(result).to.contain('foo <img class="habitica-emoji" style="height: 1.5em; width: 1.5em" src="https://s3.amazonaws.com/habitica-assets/cdn/emoji/smile.png" alt="smile"> bar');
   });
 
-  xit('renders unicode emoji from s3', function () {
+  it('renders unicode emoji from s3', function () {
     let result = md.render('foo 👍 bar');
 
     expect(result).to.contain('foo <img class="habitica-emoji" style="height: 1.5em; width: 1.5em" src="https://s3.amazonaws.com/habitica-assets/cdn/emoji/thumbsup.png" alt="thumbsup"> bar');
   });
 
-  xit('renders shortcut emoji from s3', function () {
+  it('renders shortcut emoji from s3', function () {
     let result = md.render('foo :) bar');
 
     expect(result).to.contain('foo <img class="habitica-emoji" style="height: 1.5em; width: 1.5em" src="https://s3.amazonaws.com/habitica-assets/cdn/emoji/smiley.png" alt="smiley"> bar');
